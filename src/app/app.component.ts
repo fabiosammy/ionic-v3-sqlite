e2f9c1fdbd9227e515113c2ebda44225a9f2aca2
fabiosammy <fabiosammy@gmail.com>
2019-05-29 21:08:27 -0300

    Added the provider methods and call in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { DatabaseProvider } from '../providers/database/database'
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -11,11 +12,12 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, dbProvider: DatabaseProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
+      dbProvider.createDatabase();
       splashScreen.hide();
     });
   }
