87b6e134e2aa8e494924693e8963a9b2284dcf64
fabiosammy <fabiosammy@gmail.com>
2019-05-30 21:14:51 -0300

    Added the load radios in the abou page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RadioProvider } from '../../providers/radio/radio'
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  radios: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private radioProvider: RadioProvider) {
 
+  }
+
+  ionViewDidLoad(){
+    this.radioProvider.all().then((data: any) => {
+      this.radios = data;
+    });
+    console.log(this.radios);
   }
 
 }
